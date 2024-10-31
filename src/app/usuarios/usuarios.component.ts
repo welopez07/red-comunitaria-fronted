0f0d38a14841bd584aec0cc959b9ae0f2c8a6df8
wilson <welopez07@gmail.com>
2024-10-30 23:57:05 -0500

    crear botones elieminar y editar de usuarios component

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { elementAt } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Usuario } from '../models/usuario.model';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+
 
 
 @Component({
@@ -16,15 +17,43 @@ export class UsuariosComponent {
   
   usuarios: Usuario[] = [
     {
+      id: 1,
       name: 'Carlos',
       lastName: 'López',
       documentNumber: '123456789',
       cellPhone: '555-1234',
       email: 'carlos@example.com',
-      role: 'Emprendedor',      
+      address: 'Calle Falsa 123',
+      birthdayDate: new Date(1990, 5, 15),
+      role: 'Emprendedor'
     },
-    
+    {
+      id: 2,
+      name: 'María',
+      lastName: 'González',
+      documentNumber: '987654321',
+      cellPhone: '555-5678',
+      email: 'maria@example.com',
+      address: 'Avenida Siempre Viva 456',
+      birthdayDate: new Date(1985, 10, 20),
+      role: 'Inversionista'
+    }    
   ];
+
+  constructor(private router: Router){}
+
+    registrarUsuario() {
+      this.router.navigate(['/registro-usuario']);
+    }
+  
+    editarUsuario(usuarioId: number) {
+      this.router.navigate(['/editar-usuario', usuarioId]);
+    }
+  
+    eliminarUsuario(usuarioId: number) {
+      // Aquí podrías añadir la lógica para eliminar al usuario
+      console.log(`Eliminar usuario con ID: ${usuarioId}`);
+    }
 }
   
 
